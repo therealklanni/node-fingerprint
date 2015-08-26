e041fff7f8eec811453e2a8c364e3dc8d2146e3c
Kevin Lanni <therealklanni@gmail.com>
2015-08-25 20:25:49 -0700

    safer process PID handling
    Accept a `pid` param when invoking `fingerprint()`

--- dist/index.js
+++ dist/index.js
@@ -5,20 +5,19 @@ Object.defineProperty(exports, '__esModule', {
 });
 var os = require('os');
 
+var padding = 2;
 var pad = function pad(str, size) {
   return (new Array(size + 1).join('0') + str).slice(-size);
 };
 
-var padding = 2;
-var pid = pad(process.pid.toString(36), padding);
 var hostname = os.hostname().split('').reduce(function (prev, char) {
   return +prev + char.charCodeAt(0);
 }, +os.hostname().length + 36).toString(36);
 
 var hostId = pad(hostname, padding);
 
-exports['default'] = function () {
-  return pid + hostId;
+exports['default'] = function (pid) {
+  return pad(pid.toString(36), padding) + hostId;
 };
 
 module.exports = exports['default'];
--- test.js
+++ test.js
@@ -6,6 +6,6 @@ let test = require('tape')
 
 test('fingerprint', (t) => {
   t.plan(2)
-  t.equal(typeof fingerprint(), 'string', 'fingerprint() should return a string');
-  t.equal(fingerprint(), fingerprint(), 'fingerprints should match');
+  t.equal(typeof fingerprint(process.pid), 'string', 'fingerprint() should return a string');
+  t.equal(fingerprint(process.pid), fingerprint(process.pid), 'fingerprints should match');
 })
